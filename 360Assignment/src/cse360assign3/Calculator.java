32b7421bbb061c274f2f227b12fb69f890662fcd
Samantha Classen <seclasse@asu.edu>
2016-02-17 22:05:21 -0700

    Initial check-in of Calculator

---
+++ 360Assignment/src/cse360assign3/Calculator.java
@@ -0,0 +1,77 @@
+package cse360assign3;
+/**
+ * This class functions just as a simple calculator does. It performs
+ * addition, subtraction, multiplication, and division. Furthermore, 
+ * it tracks all past calculations.
+ * 
+ * @author Samantha Classen (PIN #: 220)
+ *
+ */
+
+public class Calculator 
+{
+	/** The running total that is added, subtracted, multiplied, and divided from. */
+	private int total;
+	
+	/**
+	 * This constructor initially has a total of 0.
+	 */
+	public Calculator ()
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This will return the total.
+	 * @return running total
+	 */
+	public int getTotal ()
+	{
+		return 0;
+	}
+	
+	/**
+	 * Adds the given value to the total.
+	 * @param value to be added
+	 */
+	public void add (int value)
+	{
+		
+	}
+	
+	/**
+	 * Subtracts the given value from the total.
+	 * @param value to be subtracted
+	 */
+	public void subtract (int value)
+	{
+		
+	}
+	
+	/**
+	 * Multiples the total by the given value.
+	 * @param value will by multiplied with the total
+	 */
+	public void multiply (int value)
+	{
+		
+	}
+	
+	/**
+	 * Divides the total by the given value.
+	 * @param value will used to divide the total
+	 */
+	public void divide (int value)
+	{
+		
+	}
+	
+	/**
+	 * Returns the history of calculations involving the total.
+	 * @return a string describing these calculations
+	 */
+	public String getHistory ()
+	{
+		return "";
+	}
+}
---
+++ 360Assignment/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,54 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest
+{
+
+	// makes sure that constructor is properly instantiated
+	@Test
+	public void testCalculator()
+	{
+		Calculator calc = new Calculator();
+		assertNotNull(calc);
+	}
+
+	@Test
+	public void testGetTotal()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubtract()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMultiply()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDivide()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHistory()
+	{
+		fail("Not yet implemented");
+	}
+
+}
